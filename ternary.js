5cf40b84cd04c19679c17eab00a9534e266f7da7
toth_szilvia <szylwy@gmail.com>
2022-03-22 16:07:56 +0100

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,10 @@
+const [min, max] = [8888, 9];
+// if (min < max) {
+//     console.log("Min is lower than max.");
+// } else {
+//     console.log("Min is higher than max");
+// }
+
+// min < max ? console.log("Min is lower than max.") : console.log("Min is higher than max.");
+
+console.log(`Min is ${min < max ? "" : "NOT"} lower than max.`);
